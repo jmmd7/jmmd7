415d71ed96e7ef4fc3560131562ddbd28b62ef72
Morris <jmmd7@umsystem.edu>
2020-01-29 13:33:27 -0600

    Add initial setup files

---
+++ Project1/Lab02Main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string name = "John";
+	float homeworkScore;
+	homeworkScore = 89.2;
+
+	cout << "Hello, " << name << endl;
+	cout << "Homework : " << homeworkScore << endl;
+}
